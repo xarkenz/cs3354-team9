60b99433e1f36cffae3abff5c2d0a8341d1e4195
Syed Hasan <ozair081@gmail.com>
2025-04-20 21:59:53 -0500

    Update main.js

--- thresh-ui/src/main.js
+++ thresh-ui/src/main.js
@@ -1,11 +1,16 @@
 import './assets/main.css'
+import './index.css'
 
 import { createApp } from 'vue'
-import App from './App.vue'
-import './index.css'
-import VueCookies from 'vue3-cookies'
+import App           from './App.vue'
+
+import router        from './router'           
+import VueCookies    from 'vue3-cookies'
 import AsyncComputed from 'vue-async-computed'
 
-const app = createApp(App);
-app.config.globalProperties.session = "";
-app.use(VueCookies, {expireTimes: "30d",}).use(AsyncComputed).mount('#app');
+const app = createApp(App)
+app
+  .use(router)            
+  .use(VueCookies, { expireTimes: '30d' })
+  .use(AsyncComputed)
+  .mount('#app')
