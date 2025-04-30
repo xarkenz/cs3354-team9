151dd01fbdb99814e04c06aa5b607fb69fc66567
IsaacP <isaacphilo1@gmail.com>
2025-04-30 13:33:29 -0500

    Added preliminary demo capacities
    The heart icon now changes color when clicked.

--- thresh-ui/src/pages/RestaurantPanel.vue
+++ thresh-ui/src/pages/RestaurantPanel.vue
@@ -108,6 +108,10 @@ function getAverageStars(reviews) {
   const total = reviews.reduce((sum, r) => sum + r.numStars, 0);
   return total / reviews.length;
 }
+
+function saveToFavorites(event){
+  document.getElementById("HeartIcon").innerHTML = "<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 24 24\" fill=\"red\" class=\"size-6\"><path d=\"m11.645 20.91-.007-.003-.022-.012a15.247 15.247 0 0 1-.383-.218 25.18 25.18 0 0 1-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0 1 12 5.052 5.5 5.5 0 0 1 16.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 0 1-4.244 3.17 15.247 15.247 0 0 1-.383.219l-.022.012-.007.004-.003.001a.752.752 0 0 1-.704 0l-.003-.001Z\" /></svg>";
+}
 </script>
 
 <template>
@@ -130,7 +134,7 @@ function getAverageStars(reviews) {
   />
   <button
     class="absolute top-2 right-2 bg-white bg-opacity-80 p-2 rounded-full shadow hover:bg-opacity-100 transition"
-    aria-label="Save to favorites"
+    aria-label="Save to favorites" @click="saveToFavorites" id = "HeartIcon"
   >
     <HeartOutlineIcon class="w-6 h-6 text-green-900" />
   </button>
